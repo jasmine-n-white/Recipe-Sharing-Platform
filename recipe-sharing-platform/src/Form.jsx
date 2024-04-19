d26ca91187a639b3b96f421997e8ad0ad66a6e08
Jasmine White <jasmine@whitepdx.com>
2024-04-18 19:00:28 -0700

    starting form validation

--- recipe-sharing-platform/src/Form.jsx
+++ recipe-sharing-platform/src/Form.jsx
@@ -1,14 +1,33 @@
 import {useState} from 'react';
 
 function Form() {
+  const [formData, setFormData] = useState({recipeName: "", ingredients: "", instructions: "", imageURL: ""});
+  const [errors, setErrors] = useState({recipeName: "", ingredients: "", instructions: "", imageURL: ""});
+  const handleRecipeName = (event) => {
+    const {value} = event.target;
+    if (value.length < 2) {
+      setErrors({ ...errors, recipeName: "Recipe name is required." });
+    } else {
+      setErrors({ ...errors, recipeName: "" });
+  }
+  setFormData({ ...formData, recipeName: value });
+}
+
   return (
     <div className="recipeForm">
     <h2>Add a Recipe 🍲😋</h2>
     <form>
-        <input id="title" name="title" type="text" placeholder="Recipe Name" required/>
-        <textarea id="ingredients" name="ingredients" type="text" placeholder="Ingredients" required/>
-        <textarea id="instructions" name="instructions" type="text" placeholder="Instructions" required/>
-        <input id="imageURL"name="imageURL" type="url" placeholder="Image URL" required/>
+        <input id="recipeName" name="recipeName" type="text" placeholder="Recipe Name" value={formData.recipeName} onChange={handleRecipeName} />
+        <textarea id="ingredients" name="ingredients" type="text" placeholder="Ingredients" value={formData.ingredients} />
+        <textarea id="instructions" name="instructions" type="text" placeholder="Instructions" value={formData.instructions} />
+        <input id="imageURL"name="imageURL" type="url" placeholder="Image URL" value={formData.imageURL} />
+        <section className="errors">
+          <ul>
+            {Object.values(errors).map((error, i) => (
+              <li key = {i}>{error}</li>
+            ))}
+          </ul>
+        </section>
         <button type="submit">Submit!</button>
     </form>
     </div>
