eb033a0c69ed1769fb22182401d79992b61d986a
Jasmine White <jasmine@whitepdx.com>
2024-04-20 22:56:58 -0700

    like button is functional now

--- recipe-sharing-platform/src/Recipe.jsx
+++ recipe-sharing-platform/src/Recipe.jsx
@@ -2,20 +2,25 @@ import { useState } from 'react'
 
 function Recipe({recipe}) {
   const [like, setLike] = useState(0);
-  const likePost = (event) => {
-    let x = like;
-    setLike(like+1);
-    return like;
-  }
+  
+  // const ingredients = recipe.ingredients;
+  // for (i = 0; i<ingredients.length; i++) {
+  //    if (ingredients[i] == "-") {
+  //    document.createElement("\n");
+  //    recipe.appendChild("\n");
+  // }
+
+  // }
+ 
   return (
    <div className="recipeCard">
     <h3>{recipe.recipeName}</h3>
+    <img id="recipeImage" src= {recipe.imageURL} alt="user image"/>
     <p>{recipe.ingredients}</p>
     <p>{recipe.instructions}</p>
-    <img src= {recipe.imageURL} alt="user image"/>
-    <div id = "likeButton">
-    <p>{like} like</p>
-    <button type="click" onClick="likePost()">Like</button>
+    <div class = "likeSection">
+    <p>{like} like(s)</p>
+    <button id="likeButton" type="click" onClick={() => setLike((like) => like+1)}><img width="50" height="50" src="https://img.icons8.com/ios-filled/50/FFFFFF/love-circled.png" alt="love-circled"/></button>
     </div>
    
 
