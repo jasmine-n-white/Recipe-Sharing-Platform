5151235e5408a2d87e78e6e06bc9d84942a5b8a0
Jasmine White <jasmine@whitepdx.com>
2024-04-20 19:54:51 -0700

    working on like button

--- recipe-sharing-platform/src/Recipe.jsx
+++ recipe-sharing-platform/src/Recipe.jsx
@@ -2,12 +2,22 @@ import { useState } from 'react'
 
 function Recipe({recipe}) {
   const [like, setLike] = useState(0);
+  const likePost = (event) => {
+    let x = like;
+    setLike(like+1);
+    return like;
+  }
   return (
    <div className="recipeCard">
     <h3>{recipe.recipeName}</h3>
     <p>{recipe.ingredients}</p>
     <p>{recipe.instructions}</p>
     <img src= {recipe.imageURL} alt="user image"/>
+    <div id = "likeButton">
+    <p>{like} like</p>
+    <button type="click" onClick="likePost()">Like</button>
+    </div>
+   
 
    </div>
   )
