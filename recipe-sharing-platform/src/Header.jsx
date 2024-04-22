be7b49a76d6b92edc2bc104fe8944838e4d04786
Jasmine White <jasmine@whitepdx.com>
2024-04-22 11:59:50 -0700

    added header logo and changing name

--- recipe-sharing-platform/src/Header.jsx
+++ recipe-sharing-platform/src/Header.jsx
@@ -3,7 +3,8 @@ function Header() {
     return (
         <>
         <header className="headerFormat">
-            <h1>Cookonnection</h1>
+            <img src="Images/Hamster.jpg" id="Hamster" alt="Hamster"/>
+            <h1>Tasty Treat Trail</h1>
          </header>
         </>
     );
